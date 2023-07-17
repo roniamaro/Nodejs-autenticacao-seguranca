06871dcd2b09d5446934e8481c35b09038b95429
roni_amaro <roni_amaro@hotmail.com>
2023-07-17 17:25:44 -0300

    Alteração módulo 1

---
+++ 5.IntroTestesEstaticos/index.js
@@ -0,0 +1,3 @@
+const somaHorasExtras = (salario, valorHorasExtra) => salario + valorHorasExtra;
+
+console.log(somaHorasExtras(100, 50));
--- Ambiente.js
+++ Ambiente.js
@@ -1,2 +1,12 @@
 NodeJs
 Git e Github
+
+5. Introduzindo testes estáticos
+npm init -y
+npm install --save-dev eslint@8.16.0 --save-exact
+npx eslint --init
+npx eslint index.js
+OU
+Instalar a extensão do ESLint no VSCode;
+Para “chamar” o Eslint no VSC e organizar seu código automaticamente, utilize o atalho ctrl + shift + P (Windows/Linux) ou cmd + shift + P (MacOs), digite Eslint e escolha a opção "Fix all auto-fixable problems" ou posicione o cursor piscante sobre alguma das linhas sublinhadas vermelhas e utilize o atalho ctrl + . para abrir o menu do Eslint e escolher “Fix all auto-fixable problems” se estiver disponível.
+
