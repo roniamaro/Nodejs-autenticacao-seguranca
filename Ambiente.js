7fd09b1a034a3227d003b7fd2dca5f919d6d2a7f
roni_amaro <roni_amaro@hotmail.com>
2024-01-30 13:50:14 -0300

    Mod 02 > Asserções

--- Ambiente.js
+++ Ambiente.js
@@ -1,6 +1,7 @@
 NodeJs
 Git e Github
 
+Mod1
 5. Introduzindo testes estáticos
 npm init -y
 npm install --save-dev eslint@8.16.0 --save-exact
@@ -10,3 +11,8 @@ OU
 Instalar a extensão do ESLint no VSCode;
 Para “chamar” o Eslint no VSC e organizar seu código automaticamente, utilize o atalho ctrl + shift + P (Windows/Linux) ou cmd + shift + P (MacOs), digite Eslint e escolha a opção "Fix all auto-fixable problems" ou posicione o cursor piscante sobre alguma das linhas sublinhadas vermelhas e utilize o atalho ctrl + . para abrir o menu do Eslint e escolher “Fix all auto-fixable problems” se estiver disponível.
 
+Mod2
+2. Asserções
+=== 'igual'
+!== 'diferente'
+throw{} 'é uma exceção'
---
+++ Mod2/2.assercoes.js
@@ -0,0 +1,9 @@
+if (2 === 2) // true
+
+if (15 > 42) // false
+
+if (2 >= 2) // true
+
+['abacaxi', 'banana', 'uva'].includes('banana') // true
+//includes verifica dentro do array se há o termo "pesquisado";
+
